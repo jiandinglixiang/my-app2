37a562fef0129178d3b0641a5c9129a00bc3f025
华 <jiandinglixiang@qq.com>
2022-05-04 22:20:53 +0800

    1

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -2,6 +2,21 @@ import React, { useState } from "react";
 import sty from "./App.module.scss";
 import Block from "../../components/Block/index";
 import CloseIcon from "@mui/icons-material/Close";
+import { Link } from "react-router-dom";
+import {
+  Avatar,
+  Badge,
+  Button,
+  Divider,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import { deepOrange, deepPurple } from "@mui/material/colors";
+import MailIcon from "@mui/icons-material/Mail";
+import { Chip } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 
 const colors: Array<string> = [
   "red",
@@ -15,40 +30,10 @@ const colors: Array<string> = [
   "gray",
 ];
 
-const colorsComponent: Array<Function> = [
-  () => {
-    return <div>red</div>;
-  },
-  () => {
-    return <div>green</div>;
-  },
-  () => {
-    return <div>blue</div>;
-  },
-  () => {
-    return <div>yellow</div>;
-  },
-  () => {
-    return <div>purple</div>;
-  },
-  () => {
-    return <div>orange</div>;
-  },
-  () => {
-    return <div>pink</div>;
-  },
-  () => {
-    return <div>brown</div>;
-  },
-  () => {
-    return <div>gray</div>;
-  },
-];
-
 function App() {
   const [animation, setAnimation]: [Array<string>, Function] = useState([]);
 
-  function enter(x: number) {
+  function enter(x: number, disable?: boolean) {
     const currently = animation[x];
     if (!currently) {
       animation[x] = `${sty.enter} ing`;
@@ -58,14 +43,20 @@ function App() {
     if (currently.includes("ing")) {
       return;
     }
-    if (currently === sty.enter) {
+    if (!disable && currently === sty.enter) {
       animation[x] = `${sty.leave} ing`;
       setAnimation([...animation]);
     }
   }
 
-  function leave(x: number) {
+  function leave(x: number, event: React.AnimationEvent) {
     const currently = animation[x];
+    const className = (event.target as Element).className;
+    if (className !== currently) {
+      // 非目标触发禁止执行
+      return;
+    }
+    console.log(event, sty.enter);
     if (currently?.includes("ing")) {
       if (currently.includes(sty.leave)) {
         animation[x] = "";
@@ -97,8 +88,9 @@ function App() {
               <div
                 style={{ backgroundColor: value }}
                 className={animation[index]}
-                onClick={() => enter(index)}
-                onAnimationEnd={() => leave(index)}
+                onClick={() => enter(index, true)}
+                onAnimationEnd={(event) => leave(index, event)}
+                id="animation-container"
               >
                 {colorsComponent[index]()}
               </div>
@@ -110,5 +102,76 @@ function App() {
     </div>
   );
 }
+
+const colorsComponent: Array<Function> = [
+  () => {
+    return (
+      <div>
+        <Button variant="contained" startIcon={<RestartAltIcon />}>
+          Reset
+        </Button>
+        <Avatar>H </Avatar>
+        <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
+        <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+        <Badge badgeContent={44} color="success">
+          <MailIcon color="action" />
+        </Badge>
+      </div>
+    );
+  },
+  () => {
+    return (
+      <div>
+        <Chip component="p" label="My custom" color="info" clickable />
+        <Divider variant="inset" />
+        <Chip component="p" label="My custom2" color="error" clickable />
+      </div>
+    );
+  },
+  () => {
+    return <div>blue</div>;
+  },
+  () => {
+    return <div>yellow</div>;
+  },
+  () => {
+    return <div>purple</div>;
+  },
+  () => {
+    return <div>orange</div>;
+  },
+  () => {
+    return <div>pink</div>;
+  },
+  () => {
+    return (
+      <div>
+        <List>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar src={`${process.env.PUBLIC_URL}/logo512.png`} />
+            </ListItemAvatar>
+            <ListItemText> My custom</ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar src={`${process.env.PUBLIC_URL}/logo192.png`} />
+            </ListItemAvatar>
+            <ListItemText secondary={<div>124124124</div>}>
+              My custom
+            </ListItemText>
+          </ListItem>
+        </List>
+      </div>
+    );
+  },
+  () => {
+    return (
+      <div>
+        <Link to="/forward2">Next Page </Link>
+      </div>
+    );
+  },
+];
 
 export default App;
